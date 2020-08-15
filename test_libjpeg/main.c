dd3639d324016c74173b993f2f8fc6d462d25e39
FS2004 <p-kun.m@nifty.com>
2020-08-16 04:51:41 +0900

    Add memory destination

--- test_libjpeg/main.c
+++ test_libjpeg/main.c
@@ -23,13 +23,25 @@
 #include <stdlib.h>
 #include <stddef.h>
 #include "jpeglib.h"
+#include "jerror.h"
+#include "png.h"
 
 /****************************************************************************
  * Pre-processor definitions
  ****************************************************************************/
 
-#define INP_FILENAME  "flower.bmp"
+#define INP_FILENAME  "../flower.bmp"
+//#define INP_FILENAME  "../zeno.bmp"
+//#define INP_FILENAME  "../pic.bmp"
 #define OUT_FILENAME  "flower.jpg"
+#define PNG_FILENAME  "../hanko.png"
+
+/****************************************************************************
+ * Pre-processor definitions
+ ****************************************************************************/
+
+#define OUTPUT_BUF_MIN_SIZE  4096
+#define OUTPUT_BUF_MAX_SIZE  60000
 
 /****************************************************************************
  * Private types
@@ -98,7 +110,7 @@ static unsigned char *read_bmp_file(const char    *filename,
   return cp;
 }
 
-/* -------------------------------------------------------------------------*/
+/*--------------------------------------------------------------------------*/
 static int write_file(const char    *filename,
                       unsigned char *bp,
                       unsigned long  size)
@@ -122,6 +134,34 @@ static int write_file(const char    *filename,
   return res;
 }
 
+/* Expanded data destination object for memory output */
+
+typedef struct {
+  struct jpeg_destination_mgr pub;        /* public fields */
+  boolean                     succeed;
+}
+my_mem_destination_mgr;
+
+typedef my_mem_destination_mgr *my_mem_dest_ptr;
+typedef my_mem_destination_mgr  my_mem_dest;
+
+/*-------------------------------------------------------------------------*/
+METHODDEF(void) mem_destination (j_compress_ptr cinfo){}
+
+/*-------------------------------------------------------------------------*/
+METHODDEF(boolean) empty_mem_output_buffer (j_compress_ptr cinfo)
+{
+  /* empty_mem_output_buffer */
+
+  my_mem_dest_ptr dest = (my_mem_dest_ptr) cinfo->dest;
+
+  dest->pub.next_output_byte = dest->pub.next_output_byte - OUTPUT_BUF_MIN_SIZE;
+  dest->pub.free_in_buffer   = OUTPUT_BUF_MIN_SIZE;
+  dest->succeed              = FALSE;
+
+  return TRUE;
+}
+
 /****************************************************************************
  * Public functions
  ****************************************************************************/
@@ -129,6 +169,8 @@ static int write_file(const char    *filename,
 unsigned char *encode(unsigned char *inptr,
                       int            width,
                       int            height,
+                      int            quality,
+                      unsigned char *outptr,
                       unsigned long *size)
 {
   struct jpeg_compress_struct jpeg;
@@ -136,16 +178,7 @@ unsigned char *encode(unsigned char *inptr,
   FILE                       *fp;
   JSAMPLE                    *addr;
   int                         comp = 3; /* RGB 24bit */
-  unsigned char              *cp;
-
-  /* Secure output destination memory */
-
-  cp = malloc(*size);
-
-  if (cp == NULL)
-    {
-      return NULL;
-    }
+  my_mem_dest                 dest;
 
   /* Initialize Jpeg encoding */
 
@@ -155,7 +188,14 @@ unsigned char *encode(unsigned char *inptr,
 
   /* Setting the output memory */
 
-  jpeg_mem_dest(&jpeg, &cp, size);
+  jpeg.dest = (struct jpeg_destination_mgr *)&dest;
+
+  dest.pub.init_destination    = mem_destination;
+  dest.pub.empty_output_buffer = empty_mem_output_buffer;
+  dest.pub.term_destination    = mem_destination;
+  dest.pub.next_output_byte    = outptr;
+  dest.pub.free_in_buffer      = *size;
+  dest.succeed                 = TRUE;
 
   /* Set encoded image information */
 
@@ -165,7 +205,7 @@ unsigned char *encode(unsigned char *inptr,
   jpeg.in_color_space   = JCS_RGB;
 
   jpeg_set_defaults(&jpeg);
-  jpeg_set_quality(&jpeg, 40, TRUE);
+  jpeg_set_quality(&jpeg, quality, TRUE);
 
   /* Start encode */
 
@@ -178,15 +218,20 @@ unsigned char *encode(unsigned char *inptr,
 
   for (int j = height - 1; j >= 0; j-- )
     {
-      ptr = inptr + j * width;
+      ptr = inptr + j * ((width + 3) & ~3);
 
       for (int i = 0; i < width; i+= comp)
         {
           /* BGR to RGB */
-
+#if 1
           img[i + 0] = *(ptr + i + 2);
           img[i + 1] = *(ptr + i + 1);
           img[i + 2] = *(ptr + i + 0);
+#else
+          img[i + 0] = rand() & 0xFF;
+          img[i + 1] = rand() & 0xFF;
+          img[i + 2] = rand() & 0xFF;
+#endif
         }
 
       jpeg_write_scanlines(&jpeg, &img, 1);
@@ -202,20 +247,62 @@ unsigned char *encode(unsigned char *inptr,
 
   jpeg_destroy_compress(&jpeg);
 
-  return cp;
+  /* Set encode size */
+
+  *size -= dest.pub.free_in_buffer;
+
+  if (dest.succeed == FALSE)
+    {
+      return NULL;
+    }
+
+  return outptr;
 }
 
-/* -------------------------------------------------------------------------*/
+/*--------------------------------------------------------------------------*/
+unsigned char *decode(char *file_name)
+{
+  FILE  *fp = fopen(file_name, "rb");
+  char   head[8];
+
+  if (fp == NULL)
+    {
+      printf("File not found.\n");
+
+      return NULL;
+    }
+
+  if (png_sig_cmp(head, 0, 8))
+    {
+      printf("PNG\n");
+    }
+  else
+    {
+      printf("NG\n");
+
+      return NULL;
+    }
+
+  fclose(fp);
+
+  return NULL;
+}
+
+/*-------------------------------------------------------------------------*/
 int main(void)
 {
   int            res = -1;
   int            width;
   int            height;
+  int            quality = 95;
   unsigned long  size;
+  unsigned char *outptr;
+  unsigned char *buffer;
   unsigned char *inptr = read_bmp_file(INP_FILENAME,
                                       &width,
                                       &height,
                                       &size);
+
   if (inptr == NULL)
     {
       printf("File not found or memory allocation failed.\n");
@@ -229,15 +316,30 @@ int main(void)
 
   /* Suppose the output memory is about half of the input */
 
-  size /= 2;
+  size = OUTPUT_BUF_MAX_SIZE;
+
+  printf("size1 = %d\n", size);
+
+  buffer = malloc(size);
 
   /* Start encoding */
 
-  unsigned char *outptr = encode(inptr,
-                                 width,
-                                 height,
-                                &size);
+  for (int i = quality; i > 1; i /= 2)
+    {
+      printf("%d\n", i);
+      outptr = encode(inptr, width, height, i, buffer, &size);
+
+      if (outptr)
+        {
+          break;
+        }
+    }
+
+  
+
   free(inptr);
+
+  printf("size2 = %d\n", size);
 
   if (outptr == NULL)
     {
@@ -262,6 +364,8 @@ int main(void)
     }
 
   free(outptr);
+
+  decode(PNG_FILENAME);
 
   return res;
 }
